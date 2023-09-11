5030722a0942075b8e4b69770b917a3460e4b263
ramezaziz <rameza906@gmail.com>
2023-09-11 18:05:07 +0300

    css and html into blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,36 @@
-@extends('components.layout')
+<x-layout>
+<x-_post-header />
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
 
-@section('content')
-    @foreach($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{$post->id}}">
-                    {{$post->title}}
-                </a>
+        </div>
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
+</x-layout>
 
-            </h1>
-            <p> By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a>  in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>
-            <div>
-                {!! $post->excerpt !!}
-            </div>
-        </article>
-    @endforeach
 
-@endsection
+{{--    @foreach($posts as $post)--}}
+{{--        <article>--}}
+{{--            <h1>--}}
+{{--                <a href="/posts/{{$post->id}}">--}}
+{{--                    {{$post->title}}--}}
+{{--                </a>--}}
+
+{{--            </h1>--}}
+{{--            <p> By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a>  in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>--}}
+{{--            <div>--}}
+{{--                {!! $post->excerpt !!}--}}
+{{--            </div>--}}
+{{--        </article>--}}
+{{--    @endforeach--}}
+
 
 
 
