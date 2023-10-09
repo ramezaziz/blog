53d8e70e0357a9fac96d3f84838f7d5ca99d0461
ramezaziz <rameza906@gmail.com>
2023-10-09 14:15:22 +0300

    merge(category, search)

--- resources/views/posts/header.blade.php
+++ resources/views/posts/header.blade.php
@@ -55,7 +55,10 @@
 {{--        </div>--}}
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-200 text-gray-900 rounded-xl px-3 py-2">
-            <form method="GET" action="#">
+            <form method="GET" action="/">
+                @if(request('category'))
+                    <input type="hidden" name="category" value="{{request('category') }}">
+                @endif
                 <input type="text"
                        name="search"
                        placeholder="Find something"
